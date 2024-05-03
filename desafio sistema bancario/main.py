80892b7cebada6dcd064b7ff7e073f492482deac
Gabriel DESKTOP <leirbagrp@gmail.com>
2024-05-03 15:31:03 -0300

    att

--- desafio sistema bancario/main.py
+++ desafio sistema bancario/main.py
@@ -1,38 +1,5 @@
 import os
 
-def deposito(valor_deposito, saldo, extrato):
-    if valor_deposito > 0:
-        saldo += valor_deposito
-        extrato += f"Depósito de R$ {valor_deposito:.2f} realizado.\n"
-    else:
-        print("Valor informado para deposito é um valor negativo. Operação inválida!")
-
-    return saldo, extrato
-
-def saque(valor_saque, saldo, numero_saques, LIMITE_SAQUES, extrato):
-    if numero_saques < LIMITE_SAQUES:
-        if valor_saque <= 500.0:
-            if saldo > valor_saque:
-                saldo -= valor_saque
-                numero_saques += 1
-                extrato += f"Saque de R$ {valor_saque:.2f} realizado.\n"
-            else:
-                print("Saldo insuficiente. Operação inválida!")
-        else:
-            print("Valor informado para saque é acima do valor permitido de R$ 500,00. Operação inválida!")
-    else:
-        print("Você atingiu o limite de saques diários. Operação inválida!")
-
-    return saldo, numero_saques, extrato
-
-def mostrar_extrato(saldo, extrato):
-    print("EXTRATO".center(20, "-"))
-    if len(extrato) > 0:
-        print(extrato)
-    else:
-        print("Nenhuma movimentação foi realizada ainda.")
-    print(f"Saldo: R$ {saldo:.2f}")
-
 if __name__ == "__main__":
     menu = """
     BEM VINDO AO BANCO PYTHON DO BRASIL
@@ -57,12 +24,32 @@ if __name__ == "__main__":
 
         if opcao == "d":
             valor_deposito = float(input("Digite o valor para depósito: "))
-            saldo, extrato = deposito(valor_deposito, saldo, extrato)
+            if valor_deposito > 0:
+                saldo += valor_deposito
+                extrato += f"Depósito de R$ {valor_deposito:.2f} realizado.\n"
+            else:
+                print("Valor informado para deposito é um valor negativo. Operação inválida!")
         elif opcao == "s":
             valor_saque = float(input("Digite o valor para saque: "))
-            saldo, numero_saques, extrato = saque(valor_saque, saldo, numero_saques, LIMITE_SAQUES, extrato)
+            if numero_saques < LIMITE_SAQUES:
+                if valor_saque <= 500.0:
+                    if saldo > valor_saque:
+                        saldo -= valor_saque
+                        numero_saques += 1
+                        extrato += f"Saque de R$ {valor_saque:.2f} realizado.\n"
+                    else:
+                        print("Saldo insuficiente. Operação inválida!")
+                else:
+                    print("Valor informado para saque é acima do valor permitido de R$ 500,00. Operação inválida!")
+            else:
+                print("Você atingiu o limite de saques diários. Operação inválida!")
         elif opcao == "e":
-            mostrar_extrato(saldo, extrato)
+            print("EXTRATO".center(20, "-"))
+            if len(extrato) > 0:
+                print(extrato)
+            else:
+                print("Nenhuma movimentação foi realizada ainda.")
+            print(f"Saldo: R$ {saldo:.2f}")
         elif opcao == "q":
             print("Obrigado por utilizar o Banco Python do Brasil!")
             break
